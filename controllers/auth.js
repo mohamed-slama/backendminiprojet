112bf75637c2aaa6038b1785495ed57ebdefd202
mohamed-slama <75334129+mohamed-slama@users.noreply.github.com>
2022-12-03 15:18:54 +0100

    sssdsd

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,17 +32,17 @@ export const login = async (req, res, next) => {
       return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
+      { id: user._id },
       process.env.JWT
     );
 
-    const { password, isAdmin, ...otherDetails } = user._doc;
+    const { password, ...otherDetails } = user._doc;
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json({ details: { ...otherDetails }, isAdmin });
+      .json({ details: { ...otherDetails } });
   } catch (err) {
     next(err);
   }
